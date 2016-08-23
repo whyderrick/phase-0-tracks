1bf6f8d7b4e57b33f736569178cfa6a2e2c72e75
Derrick Carr <derrick.the.future@gmail.com>
2016-08-23 13:34:00 -0700

    Add summary string for hamster intake

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -2,7 +2,7 @@ def intake_hamster
   puts "What your hamster's name?"
   ham_name = gets.chomp
   puts "Does your hamster turn up? (Answer 1-10)"
-  turn_up = get.chomp.to_i
+  turn_up = gets.chomp.to_i
   puts "What color is your hamster?"
   ham_color = gets.chomp
   puts "Is your hamster a good candidate for adoption? (True, False)"
@@ -10,6 +10,15 @@ def intake_hamster
   adoption = true if adoption == "True"
   adoption = false if adoption == "False"
   puts "How old would you guess your hamster is?"
-  estimated_age = gets.chomp.to_i
-  estimated_age = nil if estimated_age == ""
+  estimated_age = gets.chomp
+  if estimated_age == ""
+    estimated_age = nil
+  end
+  if adoption == true
+    puts "Your hamster is name #{ham_name}. Its turn-up ratio is #{turn_up}. Its color is #{ham_color}. Its age is #{estimated_age}."
+  else
+    puts "Your hamster's no good for adoption. It's this old: #{estimated_age}."
+  end
 end
+
+intake_hamster
