bb6d2e0a9448bd2d6fc1a18dead2fd7f9d64aee0
Derrick Carr <derrick.the.future@gmail.com>
2016-08-25 00:34:02 -0700

    Reframed intake questions to give boolean values

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -9,19 +9,29 @@ def new_employee_intake
       string = string_to_bool(gets.chomp)
     end
   end
-
+  # Ask name
   puts "What is your name?"
   name = gets.chomp
+  # Ask age
   puts "How old are you?"
   age = gets.chomp.to_i
+  # Ask birthyear
   puts "What year were you born?"
   birth_year = gets.chomp.to_i
+  # Check that age and birthyear make sense together
+  if age == (2016 - birth_year)
+    # I'd love to use the Ruby Date Object here, but couldn't work it out quickly enough
+    true_age = true
+  else
+    true_age = false
+  end
+  # Check if they'll eat garlic
   puts "Should we order you garlic bread?"
-  garlic_bread = gets.chomp
+  garlic_bread = string_to_bool(gets.chomp)
   puts "Would you like to enroll in the company health plan?"
-  health_care= gets.chomp
+  health_care= string_to_bool(gets.chomp)
 
-  if age == (2016 - birth_year) && garlic_bread
+  if t && garlic_bread
     "Probably not a vampire"
   elsif !(age == (2016 - birth_year)) && (garlic_bread || garlic_bread)
     "Probably a vampire"
