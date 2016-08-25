6e26b6e98327b92ff27a6fe01c746f3c970232bc
Derrick Carr <derrick.the.future@gmail.com>
2016-08-25 00:25:11 -0700

    Add logic to convert user input to booleans

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,4 +1,15 @@
 def new_employee_intake
+  def string_to_bool(string)
+    if string.downcase == "true"
+      true
+    elsif string.downcase == "false"
+      false
+    else
+      puts "Please enter \"True\" or \"False\"."
+      string = string_to_bool(gets.chomp)
+    end
+  end
+
   puts "What is your name?"
   name = gets.chomp
   puts "How old are you?"
@@ -9,4 +20,10 @@ def new_employee_intake
   garlic_bread = gets.chomp
   puts "Would you like to enroll in the company health plan?"
   health_care= gets.chomp
+
+  if age == (2016 - birth_year) && garlic_bread
+    "Probably not a vampire"
+  elsif !(age == (2016 - birth_year)) && (garlic_bread || garlic_bread)
+    "Probably a vampire"
+  end
 end
