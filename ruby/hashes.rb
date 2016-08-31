aae25510d200072ba8364e347996dffba1f9eb9d
Derrick Carr <derrick.the.future@gmail.com>
2016-08-30 23:54:56 -0700

    Add pseudocode for hashes.rb

---
+++ ruby/hashes.rb
@@ -0,0 +1,22 @@
+=begin
+Pseudocode for interior design
+1. Prompt for customer name
+2. Create new hash
+3. Prompt user for the following types of input
+  - client age
+  - client income
+  - client's inspirations (stored as array)
+  - repeat client?
+  - house type
+  - budget
+4. Store user answers in a hash for that customer.
+5. Print the data to the screen
+6. Ask user to find mistakes
+  - If there's one,
+  --  ask for the field that has an error
+  --  convert that user string to a symbol
+  --  update the has at the key for that symbol
+7. Print the updated version
+8. Exit
+
+=end
