17d6ef5693e1ac439faa25bb12ef27775289c7a6
Derrick Carr <derrick.the.future@gmail.com>
2016-08-31 00:04:54 -0700

    Add statements to collect user input.

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -16,7 +16,30 @@ Pseudocode for interior design
   --  ask for the field that has an error
   --  convert that user string to a symbol
   --  update the has at the key for that symbol
+  - Otherwise ask user to enter "done" and exit
 7. Print the updated version
 8. Exit
 
 =end
+
+def get_info  #prompting user for info
+  puts "What's your client's name?"
+  client_name = gets.chomp
+  puts "How old is your client?"
+  client_age = gets.chomp.to_i
+  puts "What's your client's income?"
+  client_income = gets.chomp.to_i
+  puts "Have you worked with this client before? (True/False)"
+  client_repeat = gets.chomp
+  puts "List the design references your client has given you (Type \"Done\" when you've finished)."
+  client_inspiration = []
+  design_input = gets.chomp
+  until design_input.downcase = "done"
+    client_inspiration << design_input
+    puts "Anything else? (Type \"Done\" if not.)"
+    design_input
+  end
+
+
+
+end
