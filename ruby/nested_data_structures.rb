ed871f3368cfbbeaa20f3aa10c4b365d7bd2548d
Derrick Carr <derrick.the.future@gmail.com>
2016-09-01 19:48:01 -0700

    Outline structure for nested_data_structure.rb.

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,19 @@
+# Build a data structure to represent the media library in my room
+
+# Video Games (PS3 Xbox360)
+# Records (format, length, favorite tracks)
+# Movies/Shows
+
+room_fun {
+  video_games = {
+    ps3 =[],
+    xbox360 = [],
+    wii = []
+  },
+  records = {
+    {},
+    {},
+    {}
+  }
+  movies = ["Samurai Champloo", "Creed"]
+}
