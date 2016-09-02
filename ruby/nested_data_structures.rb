cfcc5d2ecb945907e26d9a821a7450744d0bf971
Derrick Carr <derrick.the.future@gmail.com>
2016-09-01 20:05:37 -0700

    Add a data record for Michael Jackson's Thriller

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -4,16 +4,24 @@
 # Records (format, length, favorite tracks)
 # Movies/Shows
 
-room_fun {
-  video_games = {
-    ps3 =["Ni No Kuni", "Devil May Cry (2013)", "Dark Souls", "Batman: Arkham City"],
-    xbox360 = ["The Witcher 2", "Eternal Sonata", "Lost Odyssey"],
-    wii = ["Okami", "The Legend of Zelda: Skyward Sword", "Punchout", "No More Heroes", "The Legend of Zelda: Twilight Princess"]
+room_fun = {
+  video_games: {
+    :ps3 =>["Ni No Kuni", "Devil May Cry (2013)", "Dark Souls", "Batman: Arkham City"],
+    :xbox360 => ["The Witcher 2", "Eternal Sonata", "Lost Odyssey"],
+    :wii => ["Okami", "The Legend of Zelda: Skyward Sword", "Punchout", "No More Heroes", "The Legend of Zelda: Twilight Princess"]
   },
   records = {
-    {},
+    :thriller => {
+      :artist => "Michael Jackson",
+      :year => 1982,
+      :primary_genre => "R&B",
+      :additional_genres => [],
+      :fav_tracks => []
+      :format => "LP"
+      },
     {},
     {}
   }
   movies = ["Samurai Champloo" , "Creed" , "Studio Ghibli", "Mad Max: Fury Road"]
 }
+
