3ee75b3118693a3390c658364a6825d0f5d1f978
Derrick Carr <derrick.the.future@gmail.com>
2016-09-02 11:31:07 -0700

    Release 1: Add method to calculate fibonaccis.

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -10,7 +10,7 @@ Take an array, an integer
 Stop
 
 Method takes array and integer as input
-Loop through the array, comparing each number to integer
+Loop through the array, comparing each number to integer search how
   - If it matches, return index at that point in array
   - Otherwise return nil.
 
@@ -24,3 +24,33 @@ def search_arr(array, n)
     idx += 1
   end
 end
+# Each broke for us. Need to research how to use it.
+
+
+=begin
+Release 1: Calculate Fibonacci Numbers
+
+Method takes an integer as input
+  Create a new array that contains the values 0 and 1 (possible array inside method = [0,1])
+  Until array length is equal to integer given
+  - Add last and second-to-last number in array together
+  - Push result to array within method
+  Return the array of sums
+=end
+
+def fibonacci(num)
+  fib_arr = [0, 1]
+  if num == 0 || num == 1
+    return fib_arr[num]
+  else
+    i = 2
+    until fib_arr.length == num
+      sum = fib_arr[-2] + fib_arr[-1]
+      fib_arr.push(sum)
+      i += 1
+    end
+  end
+  fib_arr
+end
+
+fibonacci(100).last == 218922995834555169026
