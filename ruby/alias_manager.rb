c98192fa8c3557961c37ba24939ca11eed3ba052
Derrick Carr <derrick.the.future@gmail.com>
2016-09-02 09:04:25 -0700

    Add pseudocode. Start working on aliasing method.

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,19 @@
+=begin Pseudocode Release 0
+Take user input for real name
+Split input into separate words
+Create a new name
+  - Change the word in position[0] to be whichever word is in position [-1]
+  - Change vowels to next vowel
+  - Change consonants to next consonant
+Return new name
+
+Time limit: 40mins
+
+=end
+
+
+def swap_names
+  name = "Felicia Torres" #expected output "Vussit Gimodoe"
+  name_arr = name.split(' ')
+
+end
