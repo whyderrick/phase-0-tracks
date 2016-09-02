c50398bd2ad498389e87269fb978047fb44bfb89
Derrick Carr <derrick.the.future@gmail.com>
2016-09-01 22:03:30 -0700

    Add additional data to music section of nested_data_structures.rb

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -10,15 +10,51 @@ room_fun = {
     :xbox360 => ["The Witcher 2", "Eternal Sonata", "Lost Odyssey"],
     :wii => ["Okami", "The Legend of Zelda: Skyward Sword", "Punchout", "No More Heroes", "The Legend of Zelda: Twilight Princess"]
   },
-  records = {
+  music: {
     :thriller => {
+      :pretty_title => "Thriller",
       :artist => "Michael Jackson",
       :year => 1982,
       :primary_genre => "R&B",
-      :additional_genres => [],
-      :fav_tracks => []
+      :additional_genres => ["Disco", "Funk", "Pop Rock", "Post–disco"],
+      :fav_tracks => ["Billie Jean", "Wanna Be Startin' Somethin'", "Baby Be Mine"],
       :format => "LP"
-      },
+    },
+    :youre_dead => {
+      pretty_title: "You're Dead",
+      artist: "Flying Lotus",
+      year: 2014,
+      primary_genre: "Experimental",
+      additional_genres: ["Hip-Hop", "Jazz Fusion"],
+      fav_tracks: ["Never Catch Me", "Tesla", "Turkey Dog Coma"],
+      format: "LP"
+    },
+    :coloring_book => {
+      pretty_title: "Coloring Book",
+      :artist => "Chance the Rapper",
+      :year => 2016,
+      :primary_genre => "Hip-Hop",
+      :additional_genres => ["Gospel", "Christian hip hop"],
+      :fav_tracks => ["How Great", "All Night", "Angels"],
+      :format => "Digital"
+    },
+    :for_all_we_know =>{
+      pretty_title: "For All We Know",
+      artist: "NAO",
+      year: 2016,
+      primary_genre: "Synthpop",
+      additional_genres: ["R&B", "Pop"],
+      fav_tracks: ["DYWM","Adore You","Happy"],
+      format: "Digital"
+    }
+  }
+}
+
+
+
+    }
+
+
     {},
     {}
   }
