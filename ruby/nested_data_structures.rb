b7e6106ab979ce8dd9f7e20d3e63a5a72fb35311
Derrick Carr <derrick.the.future@gmail.com>
2016-09-01 19:52:32 -0700

    Add data for video games hash

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -6,14 +6,14 @@
 
 room_fun {
   video_games = {
-    ps3 =[],
-    xbox360 = [],
-    wii = []
+    ps3 =["Ni No Kuni", "Devil May Cry (2013)", "Dark Souls", "Batman: Arkham City"],
+    xbox360 = ["The Witcher 2", "Eternal Sonata", "Lost Odyssey"],
+    wii = ["Okami", "The Legend of Zelda: Skyward Sword", "Punchout", "No More Heroes", "The Legend of Zelda: Twilight Princess"]
   },
   records = {
     {},
     {},
     {}
   }
-  movies = ["Samurai Champloo", "Creed"]
+  movies = ["Samurai Champloo" , "Creed" , "Studio Ghibli", "Mad Max: Fury Road"]
 }
