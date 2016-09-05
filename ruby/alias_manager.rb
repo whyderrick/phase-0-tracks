b1cf261a2ff0ca7b08236c42d7ff653ae7f97752
Derrick Carr <derrick.the.future@gmail.com>
2016-09-04 17:03:03 -0700

    Add scramble functionality to shift_letters method.

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -17,22 +17,29 @@ $new_name_arr = []
 
 def swap_names
   name_arr = $name.split(' ')
-  name_arr.each { |e| $new_name_arr << e}
+  name_arr.each { |e| $new_name_arr << e.downcase}
   $new_name_arr[0] = name_arr[-1]
   $new_name_arr[-1] = name_arr[0]
   $new_name_arr
 end
 
-
-
 def shift_letters(array)
-  alphabet = (a..z).to_a
-  vowels = alphabet.select { |char| char =~ /aeiou/ }
+  alphabet = ("a".."z").to_a
+  vowels = alphabet.select { |char| char =~ /[aeiou]/ }
   consonants = alphabet.select {|char| char =~ /[^aeiou]/}
-  letters_arr = []
   letters_arr = array.map do |letter|
-    if letter ~=
+    if vowels.include? letter
+      vowels[vowels.index(letter) + 1]
+    elsif consonants.include? letter
+      consonants[consonants.index(letter) + 1]
+    else
+      puts "This isn't a letter"
+    end
   end
+  letters_arr
+end
 
+# This is the method that will provide the user interface.
+def cipher
 end
 
