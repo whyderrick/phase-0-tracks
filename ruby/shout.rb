fca7ba6e9436302bca88a93d5a0ddeb1f8813889
Derrick Carr <derrick.the.future@gmail.com>
2016-09-09 09:04:21 -0700

    Convert Shout from standalone method to mixin

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,13 +1,22 @@
 module Shout
-  #We'll add some methods later.
-  def self.yell_angrily(words)
+  def yell_angrily(words)
     words + "!!!" + " :("
   end
 
-  def self.yell_happily(words)
+  def yell_happily(words)
     words + "!!!" + " <3"
   end
 end
 
-p Shout.yell_happily("I'm in love")
-p Shout.yell_angrily("My love has left me cuckolded")
+class Child
+  include Shout
+end
+
+class Trainer
+  include Shout
+end
+
+child = Child.new
+p child.yell_happily("I'm in love")
+drill_sargeant = Trainer.new
+p drill_sargeant.yell_angrily("My love has left me cuckolded")
