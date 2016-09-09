037aee4f73c30bab5b70160352e6d10405240d07
Derrick Carr <derrick.the.future@gmail.com>
2016-09-09 15:28:59 -0700

    Move pseudocode to its own text file. Write initial test names for game_spec.rb.

---
+++ ruby/game/game_spec.rb
@@ -0,0 +1,28 @@
+require_relative 'game'
+
+describe Game do
+  let(:game) {Game.new("dragoon")}
+  # it "Takes a word as input" do
+  #   expect(game.word) ==
+  # end
+
+  it "Creates an empty string of hyphens that matches the word length" do
+    #empty
+  end
+
+  it "Adds previous guesses to an array" do
+    #empty
+  end
+
+  it "doesn't penalize repeat guesses" do
+    #empty
+  end
+
+  it "Updates the number of guesses" do
+    #empty
+  end
+
+  it "Updates hyphen string " do
+    #empty
+  end
+end
