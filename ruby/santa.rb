47df51b9bd535b0050da8e52d63640e6e6ad273c
Derrick Carr <derrick.the.future@gmail.com>
2016-09-08 20:02:25 -0700

    Add additional driver code to initialize random Santas.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -5,7 +5,19 @@ class Santa
   def eat_milk_and_cookies(cookie_type)
     puts "That was a good #{cookie_type}!"
   end
-  def initialize
+  def initialize(gender, ethnicity)
     puts "Initializing Santa instance ..."
+    @gender = gender
+    @ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
   end
+end
+
+# Driver code
+santas = []
+sample_names = ["MAERON", "MAERYN","MAGGIE" ,"MAHALA", "MAHALIA", "MAHARI","MAJ","MAJOR","MAKAILA","MALACHI","MARISOL","MAURA","MAUREEN","MAURICE", "MAVERICK"]
+sample_genders = ["agender", "androgyne", "bigender", "butch", "demiboy", "demigender", "femme", "genderfluid", "genderqueer","intergender","nonbinary","male", "female"]
+10.times do
+  santas << Santa.new(sample_names.sample, sample_genders.sample)
 end
