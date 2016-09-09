8fe0e9614e23c765de258c1436aa25240477a512
Derrick Carr <derrick.the.future@gmail.com>
2016-09-09 08:51:35 -0700

    Update Shout module with a few singleton methods.

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,13 @@
 module Shout
   #We'll add some methods later.
+  def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+  def self.yell_happily(words)
+    words + "!!!" + " <3"
+  end
 end
+
+p Shout.yell_happily("I'm in love")
+p Shout.yell_angrily("My love has left me cuckolded")
