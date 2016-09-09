67bf6d4243695209e4aa911302309c2e26e195e3
Derrick Carr <derrick.the.future@gmail.com>
2016-09-08 18:14:02 -0700

    Initial commit. Create Santa class and provide starter methods.

---
+++ ruby/santa.rb
@@ -0,0 +1,11 @@
+class Santa
+  def speak
+    puts "Ho, ho, ho! Haaaappy holidays!"
+  end
+  def eat_milk_and_cookies(cookie_type)
+    puts "That was a good #{cookie_type}!"
+  end
+  def initialize
+    puts "Initializing Santa instance ..."
+  end
+end
