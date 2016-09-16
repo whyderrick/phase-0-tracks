e2402d2c8eebed53209e3868099d875610609fc6
Derrick Carr <derrick.the.future@gmail.com>
2016-09-16 12:56:55 -0700

    Iterate through names and colors to create horses.

--- js/data_structures.js
+++ js/data_structures.js
@@ -2,4 +2,18 @@ var colors = ["baby blue", "royal green", "burnt orange", "black"];
 var names = ["Ed", "Hands", "Seabiscuit", "Secretariat"];
 
 colors.push("silver");
-names.push("Silver")
+names.push("Silver");
+
+// Let's declare an object:
+// 2 different loops.
+//for each name array  -- create a new obj w. key=== name CurrentIndex , and the value equal to color at the CurrentIndex
+var horses = []
+for(var i = 0; i < names.length; i++) {
+  var horse = {};
+  horse[names[i]] = colors[i];
+  horses.push(horse)
+};
+//This doesn't currently store the horses we make, but it could
+console.log(horses);
+console.log(horses[2]);
+console.log(horses[10]);
