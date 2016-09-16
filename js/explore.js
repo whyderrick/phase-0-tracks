d971b6b1d0c129eb044bbb46a19a103f8d683e95
Derrick Carr <derrick.the.future@gmail.com>
2016-09-15 17:11:38 -0700

    Initial commit. Create explore.js and add pseudocode for reverse function.

---
+++ js/explore.js
@@ -0,0 +1,6 @@
+// Reverse a string in steps
+
+// Accept a string as input; store it as a variable
+// Create an empty string to hold the reversed word
+// Starting at the end of the input word, loop through through the word, to the beginning
+// - add each letter of the word to the new string.
