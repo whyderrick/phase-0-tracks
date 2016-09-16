3535b681ae8b9b8827f3d8cb86b607f43cb0f9d0
Derrick Carr <derrick.the.future@gmail.com>
2016-09-15 17:14:21 -0700

    Add a basic reverse function.

--- js/explore.js
+++ js/explore.js
@@ -4,3 +4,13 @@
 // Create an empty string to hold the reversed word
 // Starting at the end of the input word, loop through through the word, to the beginning
 // - add each letter of the word to the new string.
+
+function reverseString(string) {
+  newString = ""
+  for (i = string.length -1; i >= 0 ; i--) {
+    newString += string[i]
+  }
+  return newString
+}
+
+console.log(reverseString("Derrick"))
