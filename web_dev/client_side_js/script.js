e4e10bcf2540a01865cead24a8326c42753a2f35
Derrick Carr <derrick.the.future@gmail.com>
2016-09-27 18:54:46 -0700

    Remove methods that aren't in use in the script.

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -3,20 +3,12 @@
   var photo2 = document.getElementsByTagName('img')[1];
   var submitButton = document.getElementsByClassName("submit")[0];
   //hide form after submit
-  function hideForm (event){
-    event.target.parentElement.hidden = true;
-  }
+
   function submit(event){
     event.preventDefault();
     event.target.parentElement.hidden = true;
   }
-  function addTitle(event) {
-    event.target.title = "Tom \& Jerry";
-  }
 
-  function changeBackground(event) {
-    event.target.changeBackground()
-  }
   photo2.addEventListener("load", addTitle);
   submitButton.addEventListener("click", submit)
 
